5be4623ca45d45d5f86717d3f5b439e4d1a7e4ba
Yann-dev <yannnanda1@gmail.com>
2025-08-27 19:26:36 +0200

    ajout du fichier depense_firebase.jsx

---
+++ src/Modules/Depense/Depense_firebase.jsx
@@ -0,0 +1,64 @@
+import { collection, deleteDoc, doc, getDoc, getDocs, setDoc } from 'firebase/firestore'
+import { db } from '../firebase/firebase'
+
+
+
+export const  NewDepense =async (datas)=>{
+  try{
+    const docRef = doc (collection(db,'Depenses'),did)
+    await setDoc (docRef,datas)
+    console.log('depense ajouter depuis !')
+    
+  }catch(err){
+    console.log('erreur d"envoi',err)
+  }
+}
+
+export const GetDepense = async()=>{
+  try{
+    const querySnapshot = await getDocs (collection(db,'Depenses'))
+    const dataList = querySnapshot.docs.map((doc)=>({
+      id:doc.id,
+      ...doc.data()
+    }))
+    return dataList
+  } catch(err){
+    console.log('erreur lors de la recuperation',err)
+    return []
+  }
+}
+
+export const UpdateDepense = async (datas,newDatas) =>{
+  try{
+    if (!datas) {
+      console.error("Ancien Depense invalide:", datas);
+      return;
+    }
+    const docRef = doc(db,'Depenses',did)
+      await updateDoc(docRef,newDatas)
+    
+    console.log('Utilisateurs modifier !');
+  } catch(err){
+    console.log('erreur lors de l/update :',err)
+  }
+}
+
+
+export const DeleteDepense = async ()=>{
+  try{
+    const docRef = doc(db,'Depenses',did)
+    const docSnapshot = await getDoc(docRef)
+    if(!docSnapshot.exists()){
+      console.log('cette Depense n"existe pas');
+      return
+    }
+    await deleteDoc(docRef)
+    console.log('departement supprime !')
+  } catch(err){
+    console.log('erreur lors du delete :',err)
+  }
+}
+
+
+
+
